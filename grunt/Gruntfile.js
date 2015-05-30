0e6eccc306ad4e9b0a8c1f54b3ddec9344535efa
RobertoPrevato <roberto.prevato@gmail.com>
2015-05-30 14:06:55 +0200

    Improves example

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -5,6 +5,39 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    concat: {
+      //main area
+      main: {
+        src: [
+          "../scripts/demo/common/components/string.js",
+          "../scripts/demo/common/components/code.js",
+          "../scripts/demo/common/components/extend.js",
+          "../scripts/demo/common/components/events.js",
+          "../scripts/demo/common/components/progress.js",
+          "../scripts/demo/common/models/model.js",
+          "../scripts/demo/dashboard/models/dashboard.js",
+          "../scripts/demo/dashboard/templates.js",
+          "../scripts/demo/app.js"
+        ],
+        dest: "../scripts/demo/main.built.js",
+      },
+
+      //example area
+      exampleArea: {
+        src: [
+          "../scripts/demo/example-area/example-area.js"
+        ],
+        dest: "../scripts/demo/example-area.built.js"
+      }/*,
+
+      anotherArea: {
+        src: [
+          "../scripts/demo/example-area-two/example-area-two.js"
+        ],
+        dest: "../scripts/demo/example-area-two.built.js"
+      }*/
+    },
+
     uglify: {
       options: {
         preserveComments: 'some'
@@ -13,12 +46,31 @@ module.exports = function (grunt) {
         files: {
           '../scripts/libs/r.min.js': ['../scripts/libs/r.js']
         }
+      },
+      demo: {
+        files: {
+          //main min file
+          "../scripts/demo/main.min.js": ["../scripts/demo/main.built.js"],
+          //example area min file
+          "../scripts/demo/example-area.min.js": ["../scripts/demo/example-area.built.js"]
+          //example another area min file
+          //"../scripts/demo/example-area-two.min.js": ["../scripts/demo/example-area-two.built.js"]
+        }
       }
-    }
+    },
+    
+    robscure: {
+      website: {
+        // these are all the built or minified files of all areas: the task will look for each module in each file and assign them new keys.
+        // for each file, a new file will be generated, with obfuscated module names
+        areas: ["../scripts/demo/main.min.js", "../scripts/demo/example-area.min.js"]
+      }
+    },
   });
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-less');
+  //grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('robscure');
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'robscure']);
 };
